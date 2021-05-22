51686ca72999a13675ddc9aaa7cbe9a95ef535bb
Laura <3987084+lauraGit@users.noreply.github.com>
2021-05-22 16:41:57 +0200

    Re-add Project Lombok by updating to 1.18.20
    This version fixes error when accessing internal jdk.compile classes

--- src/main/java/org/udg/trackdev/spring/entity/ErrorEntity.java
+++ src/main/java/org/udg/trackdev/spring/entity/ErrorEntity.java
@@ -1,34 +1,12 @@
 package org.udg.trackdev.spring.entity;
 
-import java.util.Objects;
+import lombok.Data;
+import lombok.NonNull;
 
+@Data
 public class ErrorEntity {
-
-  public ErrorEntity(String timestamp, Integer status, String error, String message) {
-    this.timestamp = Objects.requireNonNull(timestamp, "timestamp must not be null");
-    this.status = Objects.requireNonNull(status, "status must not be null");
-    this.error = Objects.requireNonNull(error, "error must not be null");
-    this.message = Objects.requireNonNull(message, "message must not be null");
-  }
-
-  private String timestamp;
-  private Integer status;
-  private String error;
-  private String message;
-
-  public String getTimestamp() {
-    return timestamp;
-  }
-
-  public Integer getStatus() {
-    return status;
-  }
-
-  public String getError() {
-    return error;
-  }
-
-  public String getMessage() {
-    return message;
-  }
+  @NonNull private String timestamp;
+  @NonNull private Integer status;
+  @NonNull private String error;
+  @NonNull private String message;
 }
--- src/main/java/org/udg/trackdev/spring/entity/IdObject.java
+++ src/main/java/org/udg/trackdev/spring/entity/IdObject.java
@@ -1,5 +1,12 @@
 package org.udg.trackdev.spring.entity;
 
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
 public class IdObject {
+
+    @NonNull
     private String id;
+
 }
