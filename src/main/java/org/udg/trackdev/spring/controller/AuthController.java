94c06c18f29ecd82e41f3d6835beef49887fe9ab
Laura <3987084+lauraGit@users.noreply.github.com>
2021-05-23 23:08:36 +0200

    Set SameSite of cookie to None if cors is enabled.

--- src/main/java/org/udg/trackdev/spring/controller/AuthController.java
+++ src/main/java/org/udg/trackdev/spring/controller/AuthController.java
@@ -3,10 +3,12 @@ package org.udg.trackdev.spring.controller;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
+import org.udg.trackdev.spring.configuration.CorsConfiguration;
 import org.udg.trackdev.spring.entity.User;
 import org.udg.trackdev.spring.entity.views.PrivacyLevelViews;
 import org.udg.trackdev.spring.configuration.JWTAuthorizationFilter;
@@ -32,6 +34,9 @@ public class AuthController extends BaseController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    CorsConfiguration corsConfiguration;
+
     @PostMapping(path="/login")
     @JsonView(PrivacyLevelViews.Private.class)
     public ResponseEntity<Map<String, Object>> login(HttpServletRequest request,
@@ -40,12 +45,22 @@ public class AuthController extends BaseController {
 
         User user = userService.matchPassword(userBody.username, userBody.password);
         String token = getJWTToken(user);
-        Cookie cookie = new Cookie("trackdev_JWT", Base64.getEncoder().withoutPadding().encodeToString(token.getBytes()));
-        cookie.setPath("/");
-        cookie.setSecure(true);
-        cookie.setHttpOnly(true);
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.addCookie(cookie);
+
+        String cookieTokenValue = Base64.getEncoder().withoutPadding().encodeToString(token.getBytes());
+
+        String sameSite = "Lax";
+        String requestOrigin = request.getHeader("Origin");
+        if(corsConfiguration.isEnabled() && corsConfiguration.isAllowedOrigin(requestOrigin)) {
+            sameSite = "None";
+        }
+        ResponseCookie cookie = ResponseCookie.from("trackdev_JWT", cookieTokenValue)
+                .path("/")
+                .secure(true)
+                .httpOnly(true)
+                .sameSite(sameSite)
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+
         return ResponseEntity.ok().body(Map.of("userdata",user,"token", token));
     }
 
