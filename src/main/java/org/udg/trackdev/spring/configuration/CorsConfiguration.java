2587aa2efea4d621587d080a2e5170f95f75ac3d
Laura <3987084+lauraGit@users.noreply.github.com>
2021-08-09 17:40:59 +0200

    Don't fail login without Origin Header

--- src/main/java/org/udg/trackdev/spring/configuration/CorsConfiguration.java
+++ src/main/java/org/udg/trackdev/spring/configuration/CorsConfiguration.java
@@ -22,6 +22,6 @@ public class CorsConfiguration {
     }
 
     public boolean isAllowedOrigin(String origin) {
-        return this.isEnabled() && origin.equals(this.allowedOrigin);
+        return this.isEnabled() && origin != null && origin.equals(this.allowedOrigin);
     }
 }
