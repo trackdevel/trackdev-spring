6b367db8c8cc1d2d3a5beb349199bb8fd1b5de3a
Ignacio Martin <natx.mc@gmail.com>
2026-01-16 15:06:57 +0100

    Add courseId field to MergePatchReport for PATCH request updates

--- src/main/java/org/trackdev/api/model/MergePatchReport.java
+++ src/main/java/org/trackdev/api/model/MergePatchReport.java
@@ -23,4 +23,5 @@ public class MergePatchReport {
     public Optional<ReportAxisType> columnType;
     public Optional<ReportElement> element;
     public Optional<ReportMagnitude> magnitude;
+    public Optional<Long> courseId;
 }
