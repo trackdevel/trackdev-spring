fc520ce1235317f4baa515574a3e87be0bc0ad4d
Ignacio Martin <natx.mc@gmail.com>
2026-01-26 16:57:15 +0100

    ``` feat: add ProfileEnumDTO for enum definitions
    Create DTO to represent profile enum definitions with id, name,
    and list of possible values
    ```

---
+++ src/main/java/org/trackdev/api/dto/ProfileEnumDTO.java
@@ -0,0 +1,15 @@
+package org.trackdev.api.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * DTO for ProfileEnum
+ */
+@Data
+public class ProfileEnumDTO {
+    private Long id;
+    private String name;
+    private List<String> values;
+}
