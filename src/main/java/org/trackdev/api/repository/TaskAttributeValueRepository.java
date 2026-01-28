c6d096be66066ad4155dabd715691062e2e2725c
Ignacio Martin <natx.mc@gmail.com>
2026-01-28 16:55:57 +0100

    feat(repository): add TaskAttributeValueRepository for task attribute value queries

---
+++ src/main/java/org/trackdev/api/repository/TaskAttributeValueRepository.java
@@ -0,0 +1,17 @@
+package org.trackdev.api.repository;
+
+import org.trackdev.api.entity.TaskAttributeValue;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskAttributeValueRepository extends BaseRepositoryLong<TaskAttributeValue> {
+    
+    List<TaskAttributeValue> findByTaskId(Long taskId);
+    
+    Optional<TaskAttributeValue> findByTaskIdAndAttributeId(Long taskId, Long attributeId);
+    
+    void deleteByTaskIdAndAttributeId(Long taskId, Long attributeId);
+    
+    void deleteByTaskId(Long taskId);
+}
