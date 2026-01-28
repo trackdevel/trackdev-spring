8fcbc4349f1cc691c10740b1c18930b5d823dbad
Ignacio Martin <natx.mc@gmail.com>
2026-01-28 16:54:56 +0100

    feat(mapper): add TaskAttributeValueMapper for entity to DTO conversion with enum support

---
+++ src/main/java/org/trackdev/api/mapper/TaskAttributeValueMapper.java
@@ -0,0 +1,31 @@
+package org.trackdev.api.mapper;
+
+import org.mapstruct.IterableMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.trackdev.api.dto.TaskAttributeValueDTO;
+import org.trackdev.api.entity.TaskAttributeValue;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TaskAttributeValueMapper {
+
+    @Named("toDTO")
+    @Mapping(target = "attributeName", source = "attribute.name")
+    @Mapping(target = "attributeType", source = "attribute.type")
+    @Mapping(target = "enumValues", expression = "java(getEnumValues(entity))")
+    TaskAttributeValueDTO toDTO(TaskAttributeValue entity);
+
+    @IterableMapping(qualifiedByName = "toDTO")
+    List<TaskAttributeValueDTO> toDTOList(List<TaskAttributeValue> entities);
+
+    default String[] getEnumValues(TaskAttributeValue entity) {
+        if (entity.getAttribute() != null && 
+            entity.getAttribute().getEnumRef() != null) {
+            return entity.getAttribute().getEnumRef().getValues().toArray(new String[0]);
+        }
+        return null;
+    }
+}
