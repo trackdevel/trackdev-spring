37fe51685d6b59214f1c52a2d741506b61480334
Ignacio Martín <natx.mc@gmail.com>
2026-02-22 21:30:59 +0100

    feat(dto): add ListItemDTO for individual LIST attribute items

---
+++ src/main/java/org/trackdev/api/dto/ListItemDTO.java
@@ -0,0 +1,13 @@
+package org.trackdev.api.dto;
+
+import lombok.Data;
+
+/**
+ * DTO for an individual list item in a LIST-type attribute.
+ */
+@Data
+public class ListItemDTO {
+    private int orderIndex;
+    private String enumValue;
+    private String stringValue;
+}
