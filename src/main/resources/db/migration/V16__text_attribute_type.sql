0cbd90a04f32825cfb3aa24a0233f061594109a9
Ignacio Martín <natx.mc@gmail.com>
2026-03-24 19:25:33 +0100

    chore(migration): add text_value column to attribute value tables

---
+++ src/main/resources/db/migration/V16__text_attribute_type.sql
@@ -0,0 +1,3 @@
+ALTER TABLE task_attribute_values ADD COLUMN text_value TEXT;
+ALTER TABLE student_attribute_values ADD COLUMN text_value TEXT;
+ALTER TABLE pull_request_attribute_values ADD COLUMN text_value TEXT;
