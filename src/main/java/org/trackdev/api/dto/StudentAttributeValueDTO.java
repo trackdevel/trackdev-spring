af9a8f3027d4789830a3817d4612407fcd36b73c
Ignacio Martín <natx.mc@gmail.com>
2026-03-25 21:50:29 +0100

    feat(dto): update student attribute value dto to add textValue field

--- src/main/java/org/trackdev/api/dto/StudentAttributeValueDTO.java
+++ src/main/java/org/trackdev/api/dto/StudentAttributeValueDTO.java
@@ -16,5 +16,6 @@ public class StudentAttributeValueDTO {
     private AttributeType attributeType;
     private AttributeAppliedBy attributeAppliedBy;
     private String value;
+    private String textValue;
     private EnumValueEntryDTO[] enumValues;
 }
